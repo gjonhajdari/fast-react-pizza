7dd56b329fc48fa461433f048c7438f06909c177
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-09-25 12:00:52 +0200

    Applied basic tailwind styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast react pizza CO.</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast react pizza CO.
+      </Link>
 
       <SearchOrder />
 
-      <p>Gjon</p>
+      <Username />
     </header>
   );
 }
