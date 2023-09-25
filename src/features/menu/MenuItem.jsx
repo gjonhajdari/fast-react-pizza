33d01ead046bb0ce399b749e6862b30b172292e0
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-09-25 23:51:50 +0200

    Styled the menu page

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,19 +1,28 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
+    <li className="flex gap-4 py-4">
       <img
         src={imageUrl}
         alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+      <div className="mt-0.5 flex flex-grow flex-col">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between text-sm">
+          {!soldOut ? (
+            <p>{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="font-medium uppercase text-stone-500">Sold out</p>
+          )}
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
