edeae18a3b773c0a584992de266c1629f726594d
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-09-27 13:40:03 +0200

    Added update order action

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 
 import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
